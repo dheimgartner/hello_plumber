2e021272bf374e76f9c96b3f2a77cd1a2662f3fd
dheimgartner <d.heimgartners@gmail.com>
2023-10-18 17:32:35 +0200

    install_requires

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='hello_plumber',
     version='0.0.1',
+    install_requires=[
+      "requests",
+    ],
     packages=find_packages('.', exclude=['tests']),
     include_package_data=True,
     scripts=['bin/example',

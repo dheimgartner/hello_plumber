6cfca381770f8c68bf2126156553cd4312c7049d
dheimgartner <d.heimgartners@gmail.com>
2023-10-19 11:14:52 +0200

    qucik test fixture

--- tests/test_consume.py
+++ tests/test_consume.py
@@ -5,12 +5,12 @@ from hello_plumber.consume import RApiConsumer
 
 def test_consume():
     with requests_mock.mock() as m:
-        expected_data = {'msg': ["The message is: 'hello'"]}
-        m.get('http://localhost:8000/echo?msg=hello', json=expected_data)
+        expected_data = {'msg': ["The message is: 'wait_for_api'"]}  # otherwise will complain about no mock address
+        m.get('http://localhost:8000/echo?msg=wait_for_api', json=expected_data)
 
         # Now, run the code you want to test
         with RApiConsumer(port=8000) as api:
-            response = requests.get('http://localhost:8000/echo?msg=hello') # does not make a real call -> but uses the mock set up
+            response = requests.get('http://localhost:8000/echo?msg=wait_for_api') # does not make a real call -> but uses the mock set up
 
             assert response.status_code == 200
             data = response.json()
